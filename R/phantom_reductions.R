3197ca541c73081e41d39a66e8a2a5b893073a8a
caseymacgibbon <cmacgibbon@smith.edu>
2025-03-11 13:40:35 -0400

    removing double colors

--- R/phantom_reductions.R
+++ R/phantom_reductions.R
@@ -94,21 +94,6 @@ c25 <- c(
 #' @export
 #' @examples
 #' create_sankey_bump(OP_flags, year, company, funding_category, n, 3)
-c25 <- c(
-  "dodgerblue2", "#E31A1C", # red
-  "green4",
-  "#6A3D9A", # purple
-  "#FF7F00", # orange
-  "gold1",
-  "skyblue2", "#FB9A99", # lt pink
-  "palegreen2",
-  "#CAB2D6", # lt purple
-  "#FDBF6F", # lt orange
-  "gray70", "khaki2",
-  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
-  "darkturquoise", "green1", "yellow3",
-  "darkorange4", "brown"
-)
 
 create_sankey_bump <- function(phantom_emissions_flags, time, ID, category, value, id_instance,
                                title = NULL, x_label = NULL, y_label = NULL, palette = c25) {
